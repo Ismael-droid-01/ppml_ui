b5b2023e22afaed5a34c09919f0c1dee8f871935
Ismael-droid-01 <gocs020101@gs.utm.mx>
2026-04-14 23:30:44 -0600

    refactor: renamed component from model-config to algorithm (routes)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import { ModelConfig } from './components/dashboard/model-config/model-config';
+import { AlgorithmComponent } from './components/algorithm/algorithm';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'model-config', component: ModelConfig },
+    { path: 'algorithm', component: AlgorithmComponent },
     { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
